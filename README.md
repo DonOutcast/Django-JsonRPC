dce8e090b19e7e350a4d2ebc1bb379ea21b2fa49
DonOutcast <sham1996@yandex.ru>
2024-06-25 16:39:57 +0300

    changed README.md

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Django-JsonRPC
 
-Для запуска проекта необходимо просто в фале settings.py добавить:
-``
+Перед запуском проекта необходимо просто в фале settings.py добавить:
+```
 CLIENT_CERT
 CLIENT_KEY
-``
+```
+Далее команда для поднятия в контейнере:
+```angular2html
+docker-compose up --build
+```
+
+Можно запустить в ручную:
+```angular2html
+poetry install
+python mange.py runserver
+```
--- src/backend/jsonrpc/views.py
+++ src/backend/jsonrpc/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 import json
 from typing import Dict, Any
 
@@ -11,37 +9,13 @@ from jsonrpc.commands import verify_jsonrpc_method
 
 
 class JsonRpcView(View):
-    """
-    Класс-представление для обработки JSON-RPC запросов.
-
-    Attrs:
-       - template_name (str): Имя шаблона для отображения веб-интерфейса JSON-RPC.
-    """
 
     template_name: str = 'jsonrpc_ui/index.html'
 
     def get(self, request, *args, **kwargs) -> render:
-        """
-        Обработчик GET запроса, отображает веб-интерфейс JSON-RPC.
-
-        Args:
-            request (HttpRequest): Объект запроса Django.
-
-        Returns:
-            render: Отрисованный HTTP ответ с шаблоном.
-        """
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs) -> JsonResponse:
-        """
-        Обработчик POST запроса JSON-RPC метода.
-
-        Args:
-            request (HttpRequest): Объект POST запроса Django.
-
-        Returns:
-            JsonResponse: JSON ответ на POST запрос с результатами вызова JSON-RPC метода.
-        """
         method: str = request.POST.get('method', '')
         params: str = request.POST.get('params', '{}')
 
